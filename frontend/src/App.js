75e92cc6480052704ac36bf0e13c46a140a9755f
alex <4lexchen5@gmail.com>
2020-12-09 11:13:06 +1100

    designed datenodes, supports infinite scrolling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,69 @@
 // import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-import {GridList, GridListTile} from "@material-ui/core";
+import {makeStyles, GridList, GridListTile} from "@material-ui/core";
+import {blueGrey} from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  datenode_container: {
+    margin: '24px',
+    height: '640px',
+    overflow: 'scroll',
+    border: `1px solid ${blueGrey[300]}`,
+    backgroundColor: blueGrey[50],
+  },
+  datenode: {
+    backgroundColor: 'white',
+    minHeight: '160px',
+    borderRadius: '6px'
+  },
+}));
 
 function App() {
-  function  date_nodes() {
-    let dayStart = 0;
-    let numWeeks = 2;
+  const classes = useStyles();
+  const [dateNodes, setDateNodes] = React.useState(dateNodesInit(-6));
+  const paginateWeeksNext = 4;
+  const paginateWeeksPrv = 3;
 
+  function dateNodesInit(dayStart) {
+    let currDate = new Date();
+    currDate.setDate(currDate.getDate() - currDate.getDay() + dayStart);
+    return newDateNodes(currDate, 4);
+  }
+
+  function newDateNodes(startDate, weeks) {
     let ret = [];
-    for (let weeks = 0; weeks < numWeeks; weeks++) {
-      for (let day = 7 * weeks + dayStart; day < 7 * (weeks + 1) + dayStart; day++) {
-        let curr_date = new Date();
-        curr_date.setDate(day - curr_date.getDay());
-        ret.push(curr_date.getDate());
-      }
+    for (let d = 0; d < 7 * weeks; d++) {
+      let newDate = new Date(startDate);
+      newDate.setDate(newDate.getDate() + d);
+      ret.push(newDate);
     }
     return ret;
   }
 
+  const handleNodePagination = event => {
+    if (event.target.scrollHeight - event.target.scrollTop === event.target.clientHeight) {
+      let nextDate = new Date(dateNodes[dateNodes.length - 1]);
+      nextDate.setDate(nextDate.getDate() + 1);
+      setDateNodes([...dateNodes, ...newDateNodes(nextDate, paginateWeeksNext)]);
+    } else if (event.target.scrollTop === 0) {
+      let prvDate = new Date(dateNodes[0]);
+      prvDate.setDate(prvDate.getDate() - 7 * paginateWeeksPrv);
+      setDateNodes([...newDateNodes(prvDate, paginateWeeksPrv), ...dateNodes]);
+      event.target.scrollTop = 1;
+    };
+  } 
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload!
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <>
-    <GridList>
-      {date_nodes().map((day, i) => (
-        <GridListTile key={i}><p>{day}</p></GridListTile>
+    <GridList cols={7} spacing={6} className={classes.datenode_container} style={{margin: ''}} onScroll={handleNodePagination}>
+      {dateNodes.map(day => (
+        <GridListTile key={day} style={{height: 'auto'}}>
+          <div className={classes.datenode}>
+            {day.getDate() === 1 ? day.toLocaleDateString('default', {month: 'long'}) : day.getDate()}
+          </div>
+        </GridListTile>
       ))}
     </GridList>
     </>
